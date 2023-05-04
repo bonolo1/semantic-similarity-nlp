45be1d026df0f4d59592440d3851f74b900ccbff
Bonolo <ramasedib@gmail.com>
2023-05-04 18:33:20 +0200

    updated semantic.py file

--- semantic.py
+++ semantic.py
@@ -1,6 +1,9 @@
 # 1. Run examples
 import spacy
 nlp = spacy.load('en_core_web_md')
+
+print("-------------Similarity Between 3 Words---------------")
+
 word1 = nlp("cat")
 word2 = nlp("monkey")
 word3 = nlp("banana")
@@ -8,12 +11,14 @@ print(word1.similarity(word2))
 print(word3.similarity(word2))
 print(word3.similarity(word1))
 
+print("-------------Similarity Between Multiple Words---------------")
 
 tokens = nlp('cat apple monkey banana ')
 for token1 in tokens:
       for token2 in tokens:
             print(token1.text, token2.text, token1.similarity(token2))
 
+print("-------------Similarity Between Sentences---------------")
 
 sentence_to_compare = "Why is my cat on the car"
 sentences = ["where did my dog go",
@@ -31,6 +36,7 @@ for sentence in sentences:
 # detected based on word/token categories (where both cat and monkey are animals), but it can pick up which animal eats which food (i.e. monkey and banana).
 # This shows that NLP and the model is able to compare similarity of objects across various attribute dimensions to determine the similarity score.
 
+print("\nThree-Word Alternative Example:")
 # 3. Other Example
 # A: Another example would be the objects: "festival", "stage" and "airport". Stage is most similar to festivals as stages are used at festivals but stages have
 # the least similarity or relationship to airports. "Airport" is a bit more similar to "festival" however, as going to festivals may entail flights/travel to the
